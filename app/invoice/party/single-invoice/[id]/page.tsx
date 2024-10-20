d0128514852f84897dc1d137940c6ec8432fff36
krishnan74 <divyakrishnan.r74@gmail.com>
2024-10-20 16:31:47 +0530

    feat: Update invoice page to display party details and invoice table

--- app/invoice/party/single-invoice/[id]/page.tsx
+++ app/invoice/party/single-invoice/[id]/page.tsx
@@ -2,9 +2,20 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { PartyInvoiceDetails, UserDetails } from "@/lib/interface";
+import { Trip } from "@/lib/interface";
 import axios from "axios";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHeader,
+  TableHead,
+  TableRow,
+} from "@/components/ui/table";
 
 const Page = () => {
   const pathname = usePathname();
@@ -14,6 +25,7 @@ const Page = () => {
     useState<PartyInvoiceDetails | null>(null);
 
   const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+  const [tripDetails, setTripDetails] = useState<Trip | null>(null);
 
   const fetchInvoiceDetails = async () => {
     try {
@@ -21,9 +33,23 @@ const Page = () => {
 
       if (response.data.message === "success") {
         setInvoiceDetails(response.data.data);
+        const invoiceResponse = await axios.get(
+          `/api/trip/${response.data.data?.tripId}`
+        );
+
+        if (invoiceResponse.data.message === "success") {
+          setTripDetails(invoiceResponse.data.data);
+        }
       }
     } catch (error) {
       console.error("An error occurred while fetching invoice details", error);
+    }
+  };
+
+  const fetchTripDetails = async () => {
+    try {
+    } catch (error) {
+      console.error("An error occurred while fetching trip details", error);
     }
   };
 
@@ -32,6 +58,7 @@ const Page = () => {
       const response = await axios.get("/api/user/1");
 
       if (response.data.message === "success") {
+        console.log(response.data.data);
         setUserDetails(response.data.data);
       }
     } catch (error) {
@@ -45,11 +72,11 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4">
+    <div className="p-4">
       {invoiceDetails ? (
         <>
-          <div className="grid grid-cols-4">
-            <div className="col-span-2 flex flex-col">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 border">
+            <div className="col-span-2 flex flex-col border p-3">
               <p>{userDetails?.companyName}</p>
               <p>{userDetails?.doorNumber}</p>
               <p>{userDetails?.street}</p>
@@ -58,66 +85,156 @@ const Page = () => {
               <p>{userDetails?.zipCode}</p>
             </div>
 
-            <div className="col-span-1 flex-col">
-              <div>
+            <div className="col-span-1 flex-col ">
+              <div className="border p-3">
                 <p>Invoice Number</p>
                 <p>{invoiceDetails.invoiceNumber}</p>
               </div>
-              <div>
+              <div className="border p-3">
                 <p>Delivery Note</p>
                 <p>{"Notes"}</p>
               </div>
-              <div>
+              <div className="border p-3">
                 <p>Reference No & Date</p>
                 <p>{"Refer"}</p>
               </div>
             </div>
 
             <div className="col-span-1 flex-col">
-              <div>
+              <div className="border p-3">
                 <p>Date</p>
                 <p>{invoiceDetails.invoiceDate}</p>
               </div>
               <div></div>
-              <div>
+              <div className="border p-3">
                 <p>Other References</p>
                 <p>{""}</p>
               </div>
             </div>
 
-            <div className="col-span-2 flex flex-col">
-              <p>{invoiceDetails?.party.companyName}</p>
-              <p>GSTIN/UIN{invoiceDetails?.party.gstNumber}</p>
-              <p>PAN{invoiceDetails?.party.PANNumber}</p>
+            <div className="col-span-2 flex flex-col p-3 border">
+              <h2>Party Details</h2>
+              <p>
+                <strong>Name:</strong> {invoiceDetails.party.name}
+              </p>
+              <p>
+                <strong>Phone:</strong> {invoiceDetails.party.phone}
+              </p>
+              <p>
+                <strong>Company Name:</strong>{" "}
+                {invoiceDetails.party.companyName}
+              </p>
+              <p>
+                <strong>GST Number:</strong> {invoiceDetails.party.gstNumber}
+              </p>
+              <p>
+                <strong>PAN Number:</strong> {invoiceDetails.party.PANNumber}
+              </p>
+              <p>
+                <strong>Opening Balance:</strong> $
+                {invoiceDetails.party.openingBalance.toFixed(2)}
+              </p>
+              <p>
+                <strong>Total Balance:</strong> $
+                {invoiceDetails.party.totalBalance.toFixed(2)}
+              </p>
+              <p>
+                <strong>Opening Balance Date:</strong>{" "}
+                {invoiceDetails.party.openingBalanceDate}
+              </p>
             </div>
 
-            <div className="col-span-1 flex-col">
-              <div>
+            <div className="col-span-1 flex-col  border">
+              <div className="border p-3">
                 <p>Invoice Number</p>
                 <p>{invoiceDetails.invoiceNumber}</p>
               </div>
-              <div>
+              <div className="border p-3">
                 <p>Delivery Note</p>
                 <p>{"Notes"}</p>
               </div>
-              <div>
+              <div className="border p-3">
                 <p>Reference No & Date</p>
                 <p>{"Refer"}</p>
               </div>
             </div>
 
-            <div className="col-span-1 flex-col">
-              <div>
+            <div className="col-span-1 flex-col ">
+              <div className="border p-3">
                 <p>Date</p>
                 <p>{invoiceDetails.invoiceDate}</p>
               </div>
               <div></div>
-              <div>
+              <div className="border p-3">
                 <p>Other References</p>
                 <p>{""}</p>
               </div>
+            </div>
+
+            <div className="col-span-2 flex flex-col p-3 border">
+              <h2>Party Details</h2>
+              <p>
+                <strong>Name:</strong> {invoiceDetails.party.name}
+              </p>
+              <p>
+                <strong>Phone:</strong> {invoiceDetails.party.phone}
+              </p>
+              <p>
+                <strong>Company Name:</strong>{" "}
+                {invoiceDetails.party.companyName}
+              </p>
+              <p>
+                <strong>GST Number:</strong> {invoiceDetails.party.gstNumber}
+              </p>
+              <p>
+                <strong>PAN Number:</strong> {invoiceDetails.party.PANNumber}
+              </p>
+              <p>
+                <strong>Opening Balance:</strong> $
+                {invoiceDetails.party.openingBalance.toFixed(2)}
+              </p>
+              <p>
+                <strong>Total Balance:</strong> $
+                {invoiceDetails.party.totalBalance.toFixed(2)}
+              </p>
+              <p>
+                <strong>Opening Balance Date:</strong>{" "}
+                {invoiceDetails.party.openingBalanceDate}
+              </p>
             </div>
           </div>
+
+          <Table className="min-w-full bg-white shadow-sm text-black overflow-hidden mt-5">
+            <TableHeader className="border">
+              <TableRow className="">
+                <TableHead className="py-3 px-4 text-left ">SI.No</TableHead>
+                <TableHead className="py-3 px-4 text-left ">
+                  Description of Goods
+                </TableHead>
+                <TableHead className="py-3 px-4 text-left ">Quantity</TableHead>
+                <TableHead className="py-3 px-4 text-left ">Rate</TableHead>
+                <TableHead className="py-3 px-4 text-left ">Per</TableHead>
+
+                <TableHead className="py-3 px-4 text-left ">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow className="">
+                <TableCell className="py-3 px-4">1</TableCell>
+                <TableCell className="py-3 px-4">
+                  {tripDetails?.material}
+                </TableCell>
+                <TableCell className="py-3 px-4">{1}</TableCell>
+                <TableCell className="py-3 px-4">
+                  {tripDetails?.partyFreightAmount}
+                </TableCell>
+
+                <TableCell className="py-3 px-4">
+                  {tripDetails?.partyFreightAmount}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
 
           <div className="flex flex-col">
             <h2>Invoice Details</h2>
@@ -135,36 +252,6 @@ const Page = () => {
             </p>
             <p>
               <strong>Balance:</strong> ${invoiceDetails.balance.toFixed(2)}
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <h2>Party Details</h2>
-            <p>
-              <strong>Name:</strong> {invoiceDetails.party.name}
-            </p>
-            <p>
-              <strong>Phone:</strong> {invoiceDetails.party.phone}
-            </p>
-            <p>
-              <strong>Company Name:</strong> {invoiceDetails.party.companyName}
-            </p>
-            <p>
-              <strong>GST Number:</strong> {invoiceDetails.party.gstNumber}
-            </p>
-            <p>
-              <strong>PAN Number:</strong> {invoiceDetails.party.PANNumber}
-            </p>
-            <p>
-              <strong>Opening Balance:</strong> $
-              {invoiceDetails.party.openingBalance.toFixed(2)}
-            </p>
-            <p>
-              <strong>Total Balance:</strong> $
-              {invoiceDetails.party.totalBalance.toFixed(2)}
-            </p>
-            <p>
-              <strong>Opening Balance Date:</strong>{" "}
-              {invoiceDetails.party.openingBalanceDate}
             </p>
           </div>
 
